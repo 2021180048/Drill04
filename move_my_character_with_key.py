a927fb42577be29af5bb2fa169f72f8bfa9b6457
minkeuncho <minkeun123@naver.com>
2023-09-25 22:42:12 +0900

    x축 y축 입력 관련 코드 작성

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -7,16 +7,46 @@ character = load_image('animation_sheet02.png')
 character_stop = load_image('animation_sheet03.png')
 
 def handle_events():
-    pass
+    global running, dir_x, dir_y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT and dir_y == 0:
+                dir_x = 1
+            elif event.key == SDLK_LEFT and dir_y == 0:
+                dir_x = -1
+            elif event.key == SDLK_UP and dir_x == 0:
+                dir_y = 1
+            elif event.key == SDLK_DOWN and dir_x == 0:
+                dir_y = -1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir_x = 0
+            elif event.key == SDLK_LEFT:
+                dir_x = 0
+            elif event.key == SDLK_UP:
+                dir_y = 0
+            elif event.key == SDLK_DOWN:
+                dir_y = 0
 
 def move_character():
     pass
 
+running = True
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+frame, dir_x, dir_y = 0, 0, 0
 while True:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     move_character()
     update_canvas()
     handle_events()
+    x += dir_x * 10
+    y += dir_y * 10
     delay(0.05)
+
 close_canvas()
